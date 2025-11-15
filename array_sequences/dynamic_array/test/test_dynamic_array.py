b426bb3d29f08b98903bb145ae78ae199fe2eae2
alvarosc2 <alvarosc2@gmail.com>
2025-11-14 22:43:11 -0600

    writing unit tests

--- array_sequences/dynamic_array/test/test_dynamic_array.py
+++ array_sequences/dynamic_array/test/test_dynamic_array.py
@@ -6,3 +6,4 @@ def test_dynamic_array_append():
     arr.append({'name': 'Alvaro Silva', 'coins': 100, 'wins': 0, 'loses': 0, 'is_sol': True})
     
     assert(arr[0]['name'], 'Alvaro Silva')
+    
