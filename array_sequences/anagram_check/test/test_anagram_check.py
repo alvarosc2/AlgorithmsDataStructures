5549947000ff770e51cb8fd808514847d8ebd44b
alvarosc2 <alvarosc2@gmail.com>
2025-11-20 20:45:38 -0600

    adding unit tests

--- array_sequences/anagram_check/test/test_anagram_check.py
+++ array_sequences/anagram_check/test/test_anagram_check.py
@@ -7,22 +7,78 @@ sys.path.insert(0, str(Path(__file__).parent.parent))
 
 from anagram_check import *
 
+class TestAnagramCheck:
+    def test_anagram_check_is_anagram(self):
+        str1 = 'Clint Eastwood'
+        str2 = 'old west action'
+
+        assert anagram_check(str1, str2) == True
+
+    def test_anagram_check_is_not_anagram(self):
+        str1 = 'Clint Eastwood'
+        str2 = 'Is not anagram'
+
+        assert anagram_check(str1, str2) == False
+
+    def test_anagram_check_str1_empty(self):
+        str1 = ''
+        str2 = 'Is not anagram'
+
+        assert anagram_check(str1, str2) == False
+
+    def test_anagram_check_str2_empty(self):
+        str1 = 'Clint Eastwood'
+        str2 = ''
+
+        assert anagram_check(str1, str2) == False
+
+    def test_anagram_check_both_strings_empty(self):
+        str1 = ''
+        str2 = ''
+
+        assert anagram_check(str1, str2) == True
 class TestAnagramCheck2:
-    def test_anagram_check2(self):
+    def test_anagram_check_is_anagram(self):
         str1 = 'Clint Eastwood'
         str2 = 'old west action'
 
         assert anagram_check2(str1, str2) == True
 
-    def test_compare_arrays(self):
+    def test_anagram_check_is_not_anagram(self):
+        str1 = 'Clint Eastwood'
+        str2 = 'Not an anagram'
+
+        assert anagram_check2(str1, str2) == False
+
+
+class TestAnagramCheck3:
+    def test_anagram_check_is_anagram(self):
+        str1 = 'Clint Eastwood'
+        str2 = 'old west action'
+
+        assert anagram_check3(str1, str2) == True
+
+    def test_anagram_check_is_not_anagram(self):
+        str1 = 'Clint Eastwood'
+        str2 = 'Not an anagram'
+
+        assert anagram_check3(str1, str2) == False
+
+class TestCompareArrays:
+    def test_compare_arrays_equal_length(self):
         arr1 = [1, 2, 3, 4, 5]
         arr2 = [1, 2, 3, 4, 5]
 
         assert compare_arrays(arr1, arr2) == True
 
-class TestAnagramCheck3:
-    def test_anagram_check3(self):
-        str1 = 'Clint Eastwood'
-        str2 = 'old west action'
+    def test_compare_arrays_different_length(self):
+        arr1 = [1, 2, 3, 4]
+        arr2 = [1, 2, 3, 4, 5]
 
-        assert anagram_check3(str1, str2) == True
+        assert compare_arrays(arr1, arr2) == False
+
+    def test_compare_arrays_different_arrays(self):
+        arr1 = [1, 2, 3, 4, 5]
+        arr2 = [6, 7, 8, 9, 0]
+
+        assert compare_arrays(arr1, arr2) == False
