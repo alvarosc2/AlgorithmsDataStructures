dfecbee704efb4660cbab088cc91433a44f03859
alvarosc2 <alvarosc2@gmail.com>
2026-01-05 22:04:05 -0600

    code in try except blocks

--- tip_calculator/tip_calculator_run.py
+++ tip_calculator/tip_calculator_run.py
@@ -1,19 +1,20 @@
 from tip_calculator import tip_calculator
 
-total_bill: float = 0.0
-tip_percentage: float = 0.0
-people_to_split: int = 1
-amount_per_person: float = 0.0
+try:
+    total_bill: float = 0.0
+    tip_percentage: float = 0.0
+    people_to_split: int = 1
+    amount_per_person: float = 0.0
 
-print("Welcome to the tip calculator!")
-print("What was the total bill?")
-total_bill = float(input("$"))
+    print("Welcome to the tip calculator!")
+    total_bill = float(input("What was the total bill? $ "))
+    tip_percentage = float(input("How much tip would you like to give? 10, 12, 15, or 20? "))
+    people_to_split = int(input("How many people to split the bill? "))
 
-print("How much tip would you like to give?")
-tip_percentage = float(input("10, 12, 15, or 20?"))
+    amount_per_person = tip_calculator(total_bill, tip_percentage, people_to_split)
 
-people_to_split = int(input("How many people to split the bill?"))
-
-amount_per_person = tip_calculator(total_bill, tip_percentage, people_to_split)
-
-print(f"Each person should pay {amount_per_person}")
+    print(f"Each person should pay: $ {amount_per_person}")
+except ValueError as e:
+    print("You should enter a number, please try again.")
+except ZeroDivisionError as e:
+    print("Who is going to pay the bill? at least one person should be paying.")
