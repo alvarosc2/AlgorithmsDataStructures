c3a4a4eb05014356a856db1c8ec44bcf47fc50d9
Alvaro Silva <alvarosc2@gmail.com>
2026-02-01 17:30:33 +0000

    Understanding nested arrays in python

--- section_10_array_sequences/nested_arrays/nested_arrays.py
+++ section_10_array_sequences/nested_arrays/nested_arrays.py
@@ -3,6 +3,7 @@ vegetables = ["Spinach", "Kale", "Tomatoes", "Celeries", "Potatoes"]
 
 dirty_dozen = [fruits, vegetables]
 
-for i in range(3):
-    for j in range(3):
-        print(dirty_dozen[i][j])
+for array in dirty_dozen:
+    for item in array:
+        print(f"{item:13}", end="")
+    print()  # New line after each row
